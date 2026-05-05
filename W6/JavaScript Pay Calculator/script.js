5cf45eb49c94c48cf776cbb10b96be0cf5ded1d7
JohsuaDev <johsualopez2@gmail.com>
2026-05-05 11:09:25 -0400

    done

--- W6/JavaScript Pay Calculator/script.js
+++ W6/JavaScript Pay Calculator/script.js
@@ -10,11 +10,15 @@ function calculatepay() {
 
     const message =
         "Gross Pay: $" + grosspay.toFixed(2) + "\n" +
-        "uncle Sam's Share: $" uncleSamsShare.toFixed(2) + "\n" +
+        "uncle Sam's Share: $" + uncleSamsShare.toFixed(2) + "\n" +
         "Net pay; $" + netpay.toFixed(2);
     alert(message);
 
-    console.log
-    console.log
-    console.log
+    console.log("Gross Pay: $" + grosspay.toFixed(2));
+    console.log("uncle Sam's Share: $" + uncleSamsShare.toFixed(2));
+    console.log("Net pay; $" + netpay.toFixed(2));
+
+    document.getElementById("output").textContent = message;
 }
+
+calculatepay();
