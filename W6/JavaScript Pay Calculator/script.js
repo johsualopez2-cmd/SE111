19e06caa2de91c88e09498b358af20856e8b0c25
JohsuaDev <johsualopez2@gmail.com>
2026-05-05 11:05:38 -0400

    halfway done

---
+++ W6/JavaScript Pay Calculator/script.js
@@ -0,0 +1,20 @@
+function calculatepay() {
+    const hourlyPay = Number(prompt("Enter hourly pay:"));
+    const hoursWorked = Number(prompt("Enter Hours worked per week:"));
+    const weeks = Number(prompt("Enter Number of weeks worked:"));
+    const taxrate = 0.20;
+
+    const grosspay = hourlyPay * hoursWorked * weeks;
+    const uncleSamsShare = grosspay * taxrate;
+    const netpay = grosspay - uncleSamsShare
+
+    const message =
+        "Gross Pay: $" + grosspay.toFixed(2) + "\n" +
+        "uncle Sam's Share: $" uncleSamsShare.toFixed(2) + "\n" +
+        "Net pay; $" + netpay.toFixed(2);
+    alert(message);
+
+    console.log
+    console.log
+    console.log
+}
