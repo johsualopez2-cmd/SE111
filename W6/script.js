cda65a47db09690bb1edfc840344f5e409861079
JohsuaDev <johsualopez2@gmail.com>
2026-05-05 09:09:29 -0400

    calculator is done

--- W6/script.js
+++ W6/script.js
@@ -8,8 +8,17 @@ function multiply(num1, num2) {
 
 function calculator() {
     const num1 = parseFloat(prompt("Enter the first number:"));
-    const num2 =
+    const num2 = parseFloat(prompt("Enter the second number:"));
+    const op = prompt("Choose operation: add or multiply");
+    if (op === "add") {
+        alert("The answer is: " + add(num1, num2));
+    } else if (op === "multiply"){
+        alert("The answer is:" + multiply(num1, num2));
+    } else {
+        alert("Invalid operation")
+    }
 }
-const op = prompt("Choose operation: add or multiply");
-// basic input request
+ 
+calculator();
+
 
