e5f53cd3f5c63d243a8f6367f86cac04ada706cd
JohsuaDev <johsualopez2@gmail.com>
2026-05-05 08:36:30 -0400

    rename and more work on calculator

---
+++ W6/script.js
@@ -0,0 +1,15 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2
+}
+
+function calculator() {
+    const num1 = parseFloat(prompt("Enter the first number:"));
+    const num2 =
+}
+const op = prompt("Choose operation: add or multiply");
+// basic input request
+
